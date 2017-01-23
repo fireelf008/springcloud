26b8c3145fa5e2c7e3fe0d6af4b63eb290a9f651
王少甫 <fireelf008@163.com>
2017-01-23 23:46:48 +0800

    no message

--- eureka-server/src/main/java/com/wsf/EurekaServerStart.java
+++ eureka-server/src/main/java/com/wsf/EurekaServerStart.java
@@ -1,53 +1,16 @@
 package com.wsf;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.HttpMessageConverter;
 
 /**
  * Created by wangshaofu on 2016/11/23.
  */
-@SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerStart extends SpringBootServletInitializer {
-
-    /**
-     * 使用fastjson替换默认json框架
-     * @return
-     */
-    @Bean
-    public HttpMessageConverters fastJsonHttpMessageConverters() {
-        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-        fastConverter.setFastJsonConfig(fastJsonConfig);
-        HttpMessageConverter<?> converter = fastConverter;
-        return new HttpMessageConverters(converter);
-    }
-
-    /**
-     * tomcat启动
-     * @param application
-     * @return
-     */
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(EurekaServerStart.class);
-    }
-
-    /**
-     * main启动
-     * @param args
-     */
+@SpringBootApplication
+public class EurekaServerStart {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerStart.class, args);
+        new SpringApplicationBuilder(EurekaServerStart.class).web(true).run(args);
     }
 }

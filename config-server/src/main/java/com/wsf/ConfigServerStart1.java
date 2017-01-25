cfc0a14b14cea7434ecc60b898c118c0c66c881f
王少甫 <fireelf008@163.com>
2017-01-25 23:04:45 +0800

    no message

--- config-server/src/main/java/com/wsf/ConfigServerStart1.java
+++ config-server/src/main/java/com/wsf/ConfigServerStart1.java
@@ -3,7 +3,6 @@ package com.wsf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
@@ -13,9 +12,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
-public class ConfigServerStart {
+public class ConfigServerStart1 {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerStart.class, args);
+        SpringApplication.run(ConfigServerStart1.class, args);
     }
 }

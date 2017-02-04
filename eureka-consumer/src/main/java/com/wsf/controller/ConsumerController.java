07b3d8f514ba72013789143560bd5cde3e01266f
王少甫 <fireelf008@163.com>
2017-02-04 18:03:37 +0800

    no message

--- eureka-consumer/src/main/java/com/wsf/controller/ConsumerController.java
+++ eureka-consumer/src/main/java/com/wsf/controller/ConsumerController.java
@@ -1,5 +1,6 @@
 package com.wsf.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,7 +17,16 @@ public class ConsumerController {
     private RestTemplate restTemplate;
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "addFallback")
     public String add() {
         return restTemplate.getForEntity("http://eureka-producer/add?a=10&b=20", String.class).getBody();
+    }
+
+    /**
+     * 断路器，通过spring cloud hystrix
+     * @return
+     */
+    public String addFallback() {
+        return "error";
     }
 }

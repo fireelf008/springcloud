09ad83046f6ff501deaa491824e313214c386bdf
王少甫 <fireelf008@163.com>
2017-03-30 13:55:26 +0800

    no message

--- eureka-consumer/src/main/java/com/wsf/controller/ConsumerController.java
+++ eureka-consumer/src/main/java/com/wsf/controller/ConsumerController.java
@@ -1,6 +1,7 @@
 package com.wsf.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,10 +17,17 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
+    private static int i = 0;
+
     @RequestMapping(value = "/add", method = RequestMethod.GET)
-    @HystrixCommand(fallbackMethod = "addFallback")
+    @HystrixCommand(fallbackMethod = "addFallback", threadPoolProperties = {}, commandProperties = {@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10")})
     public String add() {
-        return restTemplate.getForEntity("http://zuul-server/eureka-producer/add?a=10&b=20", String.class).getBody();
+        System.out.println(i++);
+        if (5 <= i && i <= 15) {
+            int i = 1/0;
+        }
+        return null;
+//        return restTemplate.getForEntity("http://zuul-server/eureka-producer/add?a=10&b=20", String.class).getBody();
     }
 
     /**
